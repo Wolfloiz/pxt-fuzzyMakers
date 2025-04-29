1246a3d66e79cc173a0c143835697ad0b014a683
Loizlabs <16240413+Wolfloiz@users.noreply.github.com>
2025-04-29 13:42:16 -0300

    Incrementando bloco de velocidade do servo

--- actuators.ts
+++ actuators.ts
@@ -1,6 +1,15 @@
 /**
  * Biblioteca de atuadores do Fuzzy bits
  */
+
+enum Vel {
+//% block="Rápido"
+High,
+//% block="Médio"
+Medium,
+//% block="Devagar"
+Slow
+}
 //% block="Atuadores" weight=200 color=#89D333 icon="\uf109"
 //% groups="['Motor CC', 'Servo Motor']"
 namespace actuators {
@@ -48,88 +57,79 @@ namespace actuators {
     *  Define o ângulo para o modo sweep do servo motor.
     * @param porta de saída da placa de expansão.
     */
-    //% blockId=angleServoMotorIO block="Servo motor, definir ângulo %deg do servo motor na porta %pin no modo sweep"
-    //% deg.min=0
-    //% deg.max=1023
+    //% blockId=angleServoMotorIO block="Servo, definir velocidade %velocidade porta %pin modo Sweep"
+    //% velocidade.min=0
+    //% velocidade.max=1023
     //% group="Servo Motor"
     //% weight=10
-    export function SetAngleServo(deg: number, pin: OutputPorts) {
-        pins.analogWritePin(pin, deg);
+    export function SetAngleServo(velocidade: number, pin: OutputPorts) {
+        pins.analogWritePin(pin, velocidade);
     }
 
     /**
     *  Define o ângulo para o modo knob do servo motor.
     * @param porta de saída da placa de expansão.
     */
-    //% blockId=angleServoKnobIO block="Servo motor, definir ângulo %deg do servo motor na porta %pin no modo knob"
-    //% deg.min=0
-    //% deg.max=1023
+    //% blockId=angleServoKnobIO block="Servo, definir posição %posicao porta %pin modo Knob"
+    //% posicao.min=0
+    //% posicao.max=1023
     //% group="Servo Motor"
     //% weight=10
-    export function SetAngleServoKnob(deg: number, pin: OutputPorts) {
-        currentAngle = deg;
-        console.log(`n actual angle: ${currentAngle}`)
-        pins.analogWritePin(pin, deg)
+    export function SetAngleServoKnob(posicao: number, pin: OutputPorts) {
+        currentAngle = posicao;
+        pins.analogWritePin(pin, posicao)
     }
-
     /**
         *  Define o ângulo e velocidade de mudança gradualmente para o modo knob do servo motor.
         * @param porta de saída da placa de expansão.
         * @param angulo de destino do servo
         * @param velocidade de movimento do servo
         */
-    //% blockId=angleServoKnobGIO block="Definir ângulo %deg de servo motor na porta %pin com velocidade %speed no modo Knob"
-    //% deg.min=0
-    //% deg.max=1023
-    //% speed.min=10 speed.max=100
+    //% blockId=angleServoSpeedKnobGIO block="Servo, definir posição %posicao porta %pin com velocidade %velocidade modo Knob"
+    //% posicao.min=0
+    //% posicao.max=1023
     //% group="Servo Motor"
     //% weight=10
-    export function SetAngleServoGradually(deg: number, pin: OutputPorts, speed: number ) {
-        let step = speed / 4;  // Define a taxa de incremento
-        let delayTime = Math.map(speed, 10, 100, 100, 0);
-        let auxCurrentAngle = currentAngle;
+    export function SetAngleServoGradually(posicao: number, pin: OutputPorts, speed: Vel ) {
         
-        while (Math.abs(auxCurrentAngle - deg)> step) {
-            if (auxCurrentAngle < deg) {
-                auxCurrentAngle += step;
-            } 
-
-            if(auxCurrentAngle > deg) {
-                auxCurrentAngle -= step;
-            }
-            pins.analogWritePin(pin, Math.map(auxCurrentAngle, 0, 1023, 0, maxServoValueAnalogWrite));
-
-            basic.pause(10 * (100 - speed));  // Ajuste de delay para controle de velocidade
+        if(speed === Vel.High)
+        {
+            pins.analogWritePin(pin, posicao)
         }
-        pins.analogWritePin(pin, deg);
-        currentAngle=deg
+
+        else if (speed === Vel.Medium) {
+            posicao = Math.constrain(posicao, 0, 1023);
+            const step = 11;
+            const delayTime = 10;
+            let angle = currentAngle
+
+            while (angle != posicao) {
+                angle += (angle < posicao) ? step : -step
+                pins.analogWritePin(
+                    pin,
+                    Math.map(angle, 0, 1023, 0, maxServoValueAnalogWrite)
+                )
+
+                basic.pause(delayTime);
+            }
+        }
+
+        else if (speed === Vel.Slow) {
+            posicao = Math.constrain(posicao, 0, 1023);
+            const step = 11;
+            const delayTime = 50;
+            let angle = currentAngle
+
+            while (angle != posicao) {
+                angle += (angle < posicao) ? step : -step
+                pins.analogWritePin(
+                    pin,
+                    Math.map(angle, 0, 1023, 0, maxServoValueAnalogWrite)
+                )
+
+                basic.pause(delayTime);
+            }
+        }
+        currentAngle = posicao
     }
-
-    // /**
-    //     *  Define o ângulo e velocidade de mudança gradualmente para o modo knob do servo motor.
-    //     * @param porta de saída da placa de expansão.
-    //     * @param angulo de destino do servo
-    //     * @param velocidade de movimento do servo
-    //     */
-    // //% blockId=angleServoSweepGIO block="Definir ângulo %deg de servo motor na porta %pin com velocidade %speed no modo Sweep"
-    // //% deg.min=0
-    // //% deg.max=1023
-    // //% group="Servo Motor"
-    // //% weight=10
-    // export function SetAngleServoSweepGradually(deg: number, pin: OutputPorts, speed: number) {
-    //     let currentAngle = pins.analogReadPin(pin);
-    //     let step = speed / 10; // Define a taxa de incremento
-
-    //     while (Math.abs(currentAngle - deg) > step) {
-    //         if (currentAngle < deg) {
-    //             currentAngle += step;
-    //         } else {
-    //             currentAngle -= step;
-    //         }
-
-    //         pins.analogWritePin(pin, currentAngle);
-    //         basic.pause(10 * (100 - speed)); // Ajuste de delay para controle de velocidade
-    //     }
-    // }
-    
 }
