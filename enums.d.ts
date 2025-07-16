fc75f1cc4230607371deecb9dfb6fd8ddecda799
Loizlabs <16240413+Wolfloiz@users.noreply.github.com>
2025-07-16 09:50:56 -0300

    adicionando enums.d.ts

---
+++ enums.d.ts
@@ -0,0 +1,128 @@
+// Auto-generated. Do not edit.
+
+
+    declare const enum sengo_vision_e
+    {
+    //% block="Color"
+    kVisionColor = 1,
+    //% block="Blob"
+    kVisionBlob = 2,
+    //% block="AprilTag"
+    kVisionAprilTag = 3,
+    //% block="Line"
+    kVisionLine = 4,
+    //% block="Learning"
+    kVisionLearning = 5,
+    //% block="Card"
+    kVisionCard = 6,
+    //% block="Face"
+    kVisionFace = 7,
+    //% block="20Class"
+    kVision20Classes = 8,
+    //% block="QrCode"
+    kVisionQrCode = 9,
+    //% block="Motion"
+    kVisionMotionDetect = 11,
+    //% blockHidden=true
+    kVisionMaxType = 12,
+    }
+
+
+    declare const enum card_label_e
+    {
+    //% block="Forward"
+    kCardForward = 1,
+    //% block="Left"
+    kCardLeft = 2,
+    //% block="Right"
+    kCardRight = 3,
+    //% block="Turn Around"
+    kCardTurnAround = 4,
+    //% block="Park"
+    kCardPark = 5,
+    //% block="Green Light"
+    kCardGreenLight = 6,
+    //% block="Red Light"
+    kCardRedLight = 7,
+    //% block="Speed 40"
+    kCardSpeed40 = 8,
+    //% block="Speed 60"
+    kCardSpeed60 = 9,
+    //% block="Speed 80"
+    kCardSpeed80 = 10,
+    }
+
+
+    declare const enum class20_label_e
+    {
+    //% block="Airplane"
+    kAirplane = 1,
+    //% block="Bicycle"
+    kBicycle = 2,
+    //% block="Bird"
+    kBird = 3,
+    //% block="Boat"
+    kBoat = 4,
+    //% block="Bottle"
+    kBottle = 5,
+    //% block="Bus"
+    kBus = 6,
+    //% block="Car"
+    kCar = 7,
+    //% block="Cat"
+    kCat = 8,
+    //% block="Chair"
+    kChair = 9,
+    //% block="Cow"
+    kCow = 10,
+    //% block="DiningTable"
+    kDiningTable = 11,
+    //% block="Dog"
+    kDog = 12,
+    //% block="Horse"
+    kHorse = 13,
+    //% block="MotorBike"
+    kMotorBike = 14,
+    //% block="Person"
+    kPerson = 15,
+    //% block="PottedPlant"
+    kPottedPlant = 16,
+    //% block="Sheep"
+    kSheep = 17,
+    //% block="Sofa"
+    kSofa = 18,
+    //% block="Train"
+    kTrain = 19,
+    //% block="TvMonitor"
+    kTvMonitor = 20,
+    }
+
+
+    declare const enum color_label_e
+    {
+    //% block="Black"
+    kColorBlack = 1,
+    //% block="White"
+    kColorWhite = 2,
+    //% block="Red"
+    kColorRed = 3,
+    //% block="Green"
+    kColorGreen = 4,
+    //% block="Blue"
+    kColorBlue = 5,
+    //% block="Yellow"
+    kColorYellow = 6,
+    }
+
+
+    declare const enum apriltag_vision_mode_e
+    {
+    //% block="36H11"
+    kVisionModeFamily36H11 = 2,
+    //% block="16H5"
+    kVisionModeFamily16H5 = 0,
+    //% block="25H9"
+    kVisionModeFamily25H9 = 1,
+    }
+
+// Auto-generated. Do not edit. Really.
